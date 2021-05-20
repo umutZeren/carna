e8cee0d78eb9a5d01ad8f387467739182cab34e3
unknown <umutzerencs@gmail.com>
2021-05-21 00:14:20 +0300

    gulp

---
+++ gulp.js
@@ -0,0 +1,14 @@
+gulp.task('sass', function () {
+	return gulp.src(SCSS_SRC)
+		.pipe(wait(500))
+		.pipe(sass({
+			outputStyle: 'compressed',
+			includePaths: ['./bower_components/susy/sass', './bower_components/breakpoint-sass/stylesheets']
+		}).on('error', sass.logError))
+		.pipe(concat('style.css'))
+		.pipe(autoprefixer({
+			browsers: ['>1%', 'last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('./dist/css'));
+});
