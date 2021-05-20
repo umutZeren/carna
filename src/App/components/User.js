bc5dd164cbfe704c8657866ea03fb9fa9e7bb8c4
unknown <umutzerencs@gmail.com>
2021-05-20 23:36:42 +0300

    s

--- src/App/components/User.js
+++ src/App/components/User.js
@@ -18,8 +18,8 @@ const Users = ({users}) =>{
             {users.email}
             </td>
             <td>
-                    <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                    <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+                    <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+                    <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
 			</td>
         </>
 
